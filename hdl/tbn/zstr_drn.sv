92de3b303f677787a1b0b36f714f96d5e0b820d3
Iztok Jeras <iztok.jeras@gmail.com>
2012-09-15 11:49:15 +0200

    changes

--- hdl/tbn/zstr_drn.sv
+++ hdl/tbn/zstr_drn.sv
@@ -1,89 +1,101 @@
-module zstr_drn #(
-  parameter BW  = 8,            // bus width
-  parameter LN  = 2,            // line number (pipeline deepth)
-  parameter LNL = $clog2(LN)    // line number logarithm (number of bits for counters)
+module zstr_src #(
+  // bus
+  parameter BW = 1,             // bus width
+  parameter XZ = 1'bx,          // bus idle state
+  // queue
+  parameter QL = 1,             // queue length
+  parameter QW = $clog2(QL)     // queue address width
 )(
   // system signals
-  input  wire           z_clk,  // system clock
-  input  wire           z_rst,  // asinchronous reset
-  // zstr signals
-  input  wire           z_vld,  // transfer valid
-  input  wire  [BW-1:0] z_bus,  // grouped bus signals
-  output wire           z_ack   // transfer acknowledge
+  input  logic          clk,    // system clock
+  input  logic          rst,    // asinchronous reset
+  // z stream signals
+  input  logic          z_vld,  // transfer valid
+  input  logic [BW-1:0] z_bus,  // grouped bus signals
+  output logic          z_rdy   // transfer ready
 );
 
-reg  [BW-1:0] mem_bus [LN-1:0];  // transfer pipeline: transfer bus data
-reg  [BW-1:0] mem_msk [LN-1:0];  // transfer pipeline: transfer bus data mask
-reg  [32-1:0] mem_dly [LN-1:0];  // transfer pipeline: transfer acknowledge delay
-reg  [LN-1:0] mem_vld;           // valid status of each fifo location
+////////////////////////////////////////////////////////////////////////////////
+// bus data queue
+////////////////////////////////////////////////////////////////////////////////
 
-reg [LNL-1:0] wpt, rpt;          // write and read pointers
+logic [BW-1:0] qf_buf [0:QL-1];
+int            qf_cnt = 0;
+int            qf_wpt = 0;
+int            qf_rpt = 0;
 
-reg  [32-1:0] dly;               // transfer acknowledge delay counter 
-
-// if there is a transfer in the pipeline use the provided delay
-assign z_ack = mem_vld[rpt] ? (mem_dly[rpt] == dly) : 1'b1;
-
-assign z_trn = z_vld & z_ack;
-
-//////////////////////////////////////////////////////////////////////////////
-// write into the transfer pipeline                                         //
-//////////////////////////////////////////////////////////////////////////////
-
-always @ (posedge z_rst)
-if (z_rst) begin
-  mem_vld <= {LN{1'b0}};
-  wpt     <= 0;
+// write new bus data into the queue
+always @(posedge rst, posedge clk)
+if (rst) begin
+  qf_cnt <= 0;
+  qf_wpt <= qf_rpt;
+end else if (z_trn) begin
+  qf_cnt <=  qf_cnt + 1;
+  qf_wpt <= (qf_wpt + 1) % QL;
 end
 
-// task for adding transfers into the pipeline
-task trn (
-  input [BW-1:0] bus,
-  input [BW-1:0] msk,
-  input [32-1:0] dly
+// read 
+task get_bus (
+  output int            sts
+  output logic [BW-1:0] bus
 );
 begin
-  mem_bus [wpt] = bus;
-  mem_msk [wpt] = msk;
-  mem_dly [wpt] = dly;
-  mem_vld [wpt] = 1'b1;
-  wpt = wpt+1;
-end  
-endtask
-
-//////////////////////////////////////////////////////////////////////////////
-// read from the transfer pipeline                                          //
-//////////////////////////////////////////////////////////////////////////////
-
-always @ (posedge z_clk, posedge z_rst)
-if (z_rst) begin
-  rpt <= 0;
-  dly <= 0;
-end else begin
-  if (z_vld) begin
-    if (z_ack) begin
-      dly <= 0;
-      if (mem_vld[rpt]) begin
-        rpt <= (rpt + 1) % LN;
-        if (mem_msk[rpt] & (mem_bus[rpt] ^ z_bus))
-          $display ("DEBUG: ERROR received %h != %h, t=%0t", z_bus, mem_bus[rpt], $time);
-        else
-          $display ("DEBUG: SUCESS received %h", z_bus);
-        mem_vld[rpt] <= 1'b0;
-      end
-    end else begin
-      dly <= dly + 1;
-    end
-  end else begin
-    dly <= 0;
+  // report queue overflow
+  sts = (qf_cnt == 0);
+  // get timing from the queue
+  if (sts) begin
+    tmg = qf_buf [qf_rpt];
+    qf_cnt =  qf_cnt - 1;
+    qf_rpt = (qf_rpt + 1) % QL;
   end
 end
+endfunction
 
-wire [BW-1:0] bus;
-wire [BW-1:0] msk;
-wire [32-1:0] dly1;
-assign bus = mem_bus[rpt];
-assign msk = mem_msk[rpt];
-assign dly1 = mem_dly[rpt];
+////////////////////////////////////////////////////////////////////////////////
+// timing queue
+////////////////////////////////////////////////////////////////////////////////
+
+logic [BW-1:0] qt_buf [0:QL-1];
+int            qt_cnt = 0;
+int            qt_wpt = 0;
+int            qt_rpt = 0;
+
+always @(posedge rst, posedge clk)
+if (rst) begin
+  qt_cnt <= 0;
+  qt_rpt <= qt_wpt;
+end else if (z_trn) begin
+  qt_cnt <=  qt_cnt - 1;
+  qt_rpt <= (qt_rpt + 1) % QL;
+end
+
+task put_dat (
+  output int            sts,  // status
+  input  logic [BW-1:0] bus   // bus data
+);
+begin
+  // report queue overflow
+  sts = (qt_cnt > QL);
+  // put new bus data into the queue
+  if (sts) begin
+    qt_buf [qt_wpt] = bus;
+    qt_cnt =  qt_cnt + 1;
+    qt_wpt = (qt_wpt + 1) % QL;
+  end
+end
+endtask
+
+////////////////////////////////////////////////////////////////////////////////
+// z stream
+////////////////////////////////////////////////////////////////////////////////
+
+// valid is active if there is data in the queue
+assign z_vld = (q_cnt > 0);
+
+// queue read pointer points to stream data
+assign z_bus = q_buf [q_rpt];
+
+// stream transfer event
+assign z_trn = z_vld & z_rdy;
 
 endmodule
--- hdl/tbn/zstr_src.sv
+++ hdl/tbn/zstr_src.sv
@@ -1,7 +1,7 @@
 module zstr_src #(
   // bus
-  parameter BW = 1,
-  parameter XZ = 1'bx,
+  parameter BW = 1,             // bus width
+  parameter XZ = 1'bx,          // bus idle state
   // queue
   parameter QL = 1,             // queue length
   parameter QW = $clog2(QL)     // queue address width
@@ -9,53 +9,103 @@ module zstr_src #(
   // system signals
   input  logic          clk,    // system clock
   input  logic          rst,    // asinchronous reset
-  // zstr signals
+  // z stream signals
   output logic          z_vld,  // transfer valid
   output logic [BW-1:0] z_bus,  // grouped bus signals
   input  logic          z_rdy   // transfer ready
 );
 
 ////////////////////////////////////////////////////////////////////////////////
-// queue
+// z stream
 ////////////////////////////////////////////////////////////////////////////////
 
-logic [BW-1:0] q_buf [0:QL-1];
-int            q_cnt = 0;
-int            q_wpt = 0;
-int            q_rpt = 0;
+logic z_trn;
+int z_tmg;
+
+// valid is active if there is data in the queue
+assign z_vld = (qd_cnt > 0);
+
+// queue read pointer points to stream data
+assign z_bus = qd_buf [qd_rpt];
+
+// stream transfer event
+assign z_trn = z_vld & z_rdy;
+
+// transfer delay counter
+always @ (posedge clk, posedge rst)
+if (rst)         z_tmg <= 0;
+else if (z_vld)  z_tmg <= z_rdy ? 0 : z_tmg + 1;
+
+////////////////////////////////////////////////////////////////////////////////
+// bus data queue
+////////////////////////////////////////////////////////////////////////////////
+
+logic [BW-1:0] qd_buf [0:QL-1];
+int qd_cnt = 0;
+int qd_wpt = 0;
+int qd_rpt = 0;
 
 // queue 
 always @(posedge rst, posedge clk)
 if (rst) begin
-  q_cnt <= 0;
-  q_rpt <= q_wpt;
+  qd_cnt <= 0;
+  qd_rpt <= qd_wpt;
 end else if (z_trn) begin
-  q_cnt <=  q_cnt - 1;
-  q_rpt <= (q_rpt + 1) % QL;
+  qd_cnt <=  qd_cnt - 1;
+  qd_rpt <= (qd_rpt + 1) % QL;
 end
 
-function int put (input logic [BW-1:0] bus);
+// 
+task put_bus (
+  output int            sts,  // status
+  input  logic [BW-1:0] bus   // bus data
+);
 begin
-  // put new data into the queue
-  q_buf [q_wpt] = bus;
-  q_cnt =  q_cnt + 1;
-  q_wpt = (q_wpt + 1) % QL;
   // report queue overflow
-  put = q_cnt > QL;
+  sts = (qd_cnt > QL);
+  // put new bus data into the queue
+  if (sts) begin
+    qd_buf [qd_wpt] = bus;
+    qd_cnt =  qd_cnt + 1;
+    qd_wpt = (qd_wpt + 1) % QL;
+  end
+end
+endtask
+
+////////////////////////////////////////////////////////////////////////////////
+// timing queue
+////////////////////////////////////////////////////////////////////////////////
+
+int qt_buf [0:QL-1];
+int qt_cnt = 0;
+int qt_wpt = 0;
+int qt_rpt = 0;
+
+// queue 
+always @(posedge rst, posedge clk)
+if (rst) begin
+  qt_cnt <= 0;
+  qt_wpt <= qt_rpt;
+end else if (z_trn) begin
+  qt_cnt <=  qt_cnt + 1;
+  qt_wpt <= (qt_wpt + 1) % QL;
+  qt_buf <= z_tmg;
+end
+
+task get_tmg (
+  output int sts
+  output int tmg
+);
+begin
+  // report queue overflow
+  sts = (qt_cnt == 0);
+  // get timing from the queue
+  if (sts) begin
+    tmg = qt_buf [qt_rpt];
+    qt_cnt =  qt_cnt - 1;
+    qt_rpt = (qt_rpt + 1) % QL;
+  end
 end
 endfunction
-
-////////////////////////////////////////////////////////////////////////////////
-// z stream
-////////////////////////////////////////////////////////////////////////////////
-
-// valid is active if there is data in the queue
-assign z_vld = (q_cnt > 0);
-
-// queue read pointer points to stream data
-assign z_bus = q_buf [q_rpt];
-
-// stream transfer event
-assign z_trn = z_vld & z_rdy;
 
 endmodule
